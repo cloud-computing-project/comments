db05d129ca09cd763b371d595e584d2fb834078f
Amela Špica <amela.spica@gmail.com>
2018-01-07 14:19:55 +0100

    first commit

--- services/src/main/java/si/fri/rso/samples/comments/services/CommentsBean.java
+++ services/src/main/java/si/fri/rso/samples/comments/services/CommentsBean.java
@@ -1,32 +1,15 @@
 package si.fri.rso.samples.comments.services;
 
-
-import com.kumuluz.ee.logs.LogManager;
-import com.kumuluz.ee.logs.Logger;
 import com.kumuluz.ee.rest.beans.QueryParameters;
 import com.kumuluz.ee.rest.utils.JPAUtils;
 import si.fri.rso.samples.comments.models.Comment;
-import si.fri.rso.samples.comments.services.config.RestProperties;
 
-import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
-import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
-import com.kumuluz.ee.discovery.annotations.DiscoverService;
-import javax.persistence.TypedQuery;
-import javax.ws.rs.InternalServerErrorException;
 import javax.ws.rs.NotFoundException;
-import javax.ws.rs.ProcessingException;
-import javax.ws.rs.WebApplicationException;
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.UriInfo;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-
 
 @ApplicationScoped
 public class CommentsBean {
